87a290eded1d20ddd3ba28e0e3af9569097f5359
bryceLH <blh22buck@gmail.com>
2017-07-30 14:44:03 -0700

    test cases working

--- src/Main_Runner.java
+++ src/Main_Runner.java
@@ -11,14 +11,14 @@ public class Main_Runner {
         System.out.println("Running...");
 
         Map<String, String> temp = new HashMap<>();
-        temp.put("1", "a");
+        temp.put("8", "h");
         temp.put("2", "b");
-        temp.put("3", "c");
         temp.put("4", "d");
         temp.put("5", "e");
+        temp.put("1", "a");
         temp.put("6", "f");
         temp.put("7", "g");
-        temp.put("8", "h");
+        temp.put("3", "c");
         temp.put("9", "i");
 
         Television tv = new Television(temp);
